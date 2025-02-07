84cb06e22d267005499bb2262bfd1784e856aafe
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2025-02-07 07:47:36 -0300

    Base Config & GUI V0.1
    - Se creo un sitio.
    - Se creo una carpeta templates para los themes (Componentes, no paginas).
    - Se creo un index (Pages).

---
+++ pn/pelicanconf.py
@@ -0,0 +1,47 @@
+AUTHOR = "quinoto"
+SITENAME = "Praderas Nativas"
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = "America/Argentina/Buenos_Aires"
+
+DEFAULT_LANG = "en"
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    ("You can modify those links in your config file", "#"),
+)
+
+# Social widget
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+
+INDEX_SAVE_AS = ""
+PAGE_PATHS = ["pages"]
+DELETE_OUTPUT_DIRECTORY = True
+THEME_TEMPLATES_OVERRIDES = ["templates"]
+TEMPLATE_PAGES = {
+    "index.html": "index.html",
+    "base.html": "templates/base.html",
+}
+JINJA_ENVIRONMENT = {
+    "extensions": ["jinja2.ext.i18n"],
+}
