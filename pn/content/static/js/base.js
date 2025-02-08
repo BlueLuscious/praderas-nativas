27bb2f68c17d3adcd509c14dce894d7631ba40ba
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2025-02-08 16:44:14 -0300

    Update --> Footer (1)

--- pn/content/static/js/base.js
+++ pn/content/static/js/base.js
@@ -5,11 +5,16 @@ window.openSideNavbar = Helpers.openSideNavbar
 
 document.addEventListener("DOMContentLoaded", () => {
 
+    // Set Styles to Nav Items
     const navItems = Array.from(document.getElementsByClassName("nav_item"))
     Helpers.activeNavItem(
         navItems,
         ["bg-primary-red_tone_3", "text-white", ],
         ["bg-white", "text-primary-red_tone_3", ]
     )
+
+    // Set Year
+    const yearFooter = document.getElementById("year_footer")
+    yearFooter.textContent = new Date().getFullYear()
 
 })
